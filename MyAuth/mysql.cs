10dc0d290ed345cfbc777d2ed92e36f9de832620
haniokasai <htek@haniokasai.com>
2016-12-28 03:01:08 +0900

    maybe mysql finished

--- MyAuth/mysql.cs
+++ MyAuth/mysql.cs
@@ -1,4 +1,5 @@
-﻿using MySql.Data.MySqlClient;
+﻿using MiNET;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -36,7 +37,7 @@ namespace MyAuth
             {
                 using (MySqlCommand command = con.CreateCommand())
                 {
-                    command.CommandText = "SELECT * FROM player WHERE name = '" + name + "'";
+                    command.CommandText = "SELECT * FROM player WHERE name = '" + name.ToLower() + "'";
                     using (MySqlDataReader reader = command.ExecuteReader())
                     {
                         while (reader.Read())
@@ -68,7 +69,7 @@ namespace MyAuth
                 {
                     using (MySqlCommand command = con.CreateCommand())
                     {
-                            command.CommandText = "UPDATE player SET  llogin = '" + ctime + "'  WHERE name = '" + name + "'";
+                            command.CommandText = "UPDATE player SET  llogin = '" + ctime + "'  WHERE name = '" + name.ToLower() + "'";
                             command.ExecuteNonQuery();
                     }
                     sqlt.Commit();
@@ -81,5 +82,170 @@ namespace MyAuth
                 return false;
             }
         }
+
+        public static Boolean setuuid(String name, String uuid, String ip)
+        {
+            try
+            {
+                using (MySqlTransaction sqlt = con.BeginTransaction())
+                {
+                    using (MySqlCommand command = con.CreateCommand())
+                    {
+                        command.CommandText = "UPDATE player SET  uuid = '" + uuid + "' ,ip = '" + ip + "'   WHERE name = '" + name.ToLower() + "'";
+                        command.ExecuteNonQuery();
+                    }
+                    sqlt.Commit();
+                }
+                return true;
+            }
+            catch (MySqlException e)
+            {
+                Class1._log.Warn("myauth.setuuid: " + e.Message);
+                return false;
+            }
+
+        }
+
+        public static void regi(Player player, string hashed)
+        { 
+            try
+            {
+                string r = null;
+                using (MySqlCommand command = con.CreateCommand())
+                {
+                    command.CommandText = "SELECT name FROM player WHERE name = '" + player.Username.ToLower() + "'";
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+
+                        while (reader.Read())
+                        {
+                            r = reader["name"].ToString();
+                        }
+                        reader.Close();
+                    }
+                }
+
+                if (r == null)
+                {
+                    using (MySqlTransaction sqlt = con.BeginTransaction())
+                    {
+                        using (MySqlCommand command = con.CreateCommand())
+                        {
+                            command.CommandText = "INSERT INTO player (name,passwd,ip,cid,flogin,llogin) VALUES ('" + player.Username.ToLower() + "','" + hashed + "','" + player.EndPoint.Address.MapToIPv4().ToString() + "','" + player.ClientUuid.ToString() + "','" + ToUnixTime(DateTime.Now.ToUniversalTime()) + "','" + ToUnixTime(DateTime.Now.ToUniversalTime()) + "');";
+                            command.ExecuteNonQuery();
+                        }
+                        sqlt.Commit();
+                    }
+                }
+            }
+            catch (MySqlException e)
+            {
+                Class1._log.Warn("myauth.regi: " + e.Message);
+            }
+        }
+
+        public static Boolean remove(String name)
+        {
+            try
+            {
+                string r = null;
+                using (MySqlCommand command = con.CreateCommand())
+                {
+                    command.CommandText = "SELECT name FROM player WHERE name = @name";
+                    command.Parameters.Add(new MySqlParameter("name", name.ToLower()));
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            r = reader["name"].ToString();
+                        }
+                        reader.Close();
+                    }
+                }
+
+                if (r == null)
+                {
+                    return false;
+                }
+                if(r.Equals(name.ToLower()))
+                {
+                    using (MySqlTransaction sqlt = con.BeginTransaction())
+                    {
+                        using (MySqlCommand command = con.CreateCommand())
+                        {
+                            command.CommandText = "DELETE  FROM player WHERE name = @name";
+                            command.Parameters.Add(new MySqlParameter("name", name.ToLower()));
+                            command.ExecuteNonQuery();
+                        }
+                        sqlt.Commit();
+                    }
+                    return true;
+                }
+            }
+            catch (MySqlException e)
+            {
+                Class1._log.Warn("myauth.remove: " + e.Message);
+            }
+            return false;
+        }
+
+
+        public static Boolean login(String name, String hashed)
+        {
+            string p = null;
+            try
+            {
+                using (MySqlCommand command = con.CreateCommand())
+                {
+                    command.CommandText = "SELECT passwd FROM player WHERE name = '" + name.ToLower() + "'";
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            p = reader["name"].ToString();
+                        }
+                        reader.Close();
+                    }
+                }
+            }
+            catch (MySqlException e)
+            {
+                Class1._log.Warn("myauth.login: " + e.Message);
+            }
+            if (p == null)
+            {
+                return false;
+            }
+            if (p.Equals(hashed))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+        public static DateTime UNIX_EPOCH = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        public static long ToUnixTime(DateTime targetTime)
+        {
+            // UTC時間に変換
+            targetTime = targetTime.ToUniversalTime();
+            // UNIXエポックからの経過時間を取得
+            TimeSpan elapsedTime = targetTime - UNIX_EPOCH;
+            // 経過秒数に変換
+            return (long)elapsedTime.TotalSeconds;
+        }
+
+        public static DateTime FromUnixTime(long unixTime)
+        {
+            // UNIXエポックからの経過秒数で得られるローカル日付
+            return UNIX_EPOCH.AddSeconds(unixTime).ToLocalTime();
+        }
+    
+
+
     }
 }
